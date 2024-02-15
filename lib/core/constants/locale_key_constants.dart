fe51bba4be7602e0e301d6395b48da90b0719a32
Yakup Emeksiz <yakup@emeksiz.me>
2024-02-15 18:10:10 +0300

    add translations

---
+++ lib/core/constants/locale_key_constants.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: lines_longer_than_80_chars, comment_references, dangling_library_doc_comments, eol_at_end_of_file
+
+/// GENERATED CODE - DO NOT MODIFY BY HAND `locale_gen.dart`
+
+import 'package:easy_localization/easy_localization.dart';
+
+abstract class LocaleKeyConstants {
+
+  static final String enteredLengthShouldBeAtLeast = 'enteredLengthShouldBeAtLeast'.tr();
+  static String publishedAtWithArgs(List<String> args) => 'publishedAt'.tr(args:args);
+  static String publisherWithArgs(List<String> args) => 'publisher'.tr(args:args);
+  static String authorsWithArgs(List<String> args) => 'authors'.tr(args:args);
+  static final String pleaseEnterSearchText = 'pleaseEnterSearchText'.tr();
+  static String pagesWithArgs(List<String> args) => 'pages'.tr(args:args);
+  static final String somethingWentWrong = 'somethingWentWrong'.tr();
+  static final String noFavoriteBooksYet = 'noFavoriteBooksYet'.tr();
+  static final String searchPlaceholder = 'searchPlaceholder'.tr();
+  static final String favoriteTitle = 'favoriteTitle'.tr();
+  static final String noBooksFound = 'noBooksFound'.tr();
+  static final String unknownText = 'unknownText'.tr();
+  static final String searchText = 'searchText'.tr();
+  static final String appTitle = 'appTitle'.tr();
+
+}
+
